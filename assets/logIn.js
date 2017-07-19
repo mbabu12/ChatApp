7960a6121f21192e45e2e0e1905fbaa8aaab0e33
Maar Babu <Home@192.168.0.101>
2017-07-20 00:48:58 +0400

    adds avatar upload on sign up

--- assets/logIn.js
+++ assets/logIn.js
@@ -1,4 +1,31 @@
+var files = [];
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#profileImage').attr('src', e.target.result);
+            object = {};
+            object.filename = input.files[0].name;
+            object.data = e.target.result;
+            files.push(object);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imageUpload").change(function(){
+    readURL(this);
+});
+
+
 $(document).ready(function(){
+  $("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+  });
   $('#enter').on('click', function(){
 
     var name = $('#name').val();
@@ -27,7 +54,7 @@ $(document).ready(function(){
 
     var name = $('#name').val();
     var password = $('#password').val();
-    var loginData = {login:'no', lname: name, lpassword:password};
+    var loginData = {login:'no', lname: name, lpassword:password, imgname: files[0].filename, imgdata : files[0].data};
 
     $.ajax({
       type: 'POST',
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,7 @@ var checkData = require('../models/checkData');
 var connection = require('../models/connection');
 const UserModel = require('../models/user');
 var roomsController = require('./roomsController');
-
+var fs = require('fs');
 var urlencodedParser = bodyparser.urlencoded({ extended: false });
 //connecting to chatdb
 connection();
@@ -18,14 +18,37 @@ module.exports = function(app){
     res.render('sign');
   });
 
+  function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
+
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+  }
+
   app.post('/', urlencodedParser, function(req,res){
     var user = new UserModel({userName:req.body.lname, password:req.body.lpassword, avatar:''});
     if(req.body.login === 'no'){
+      user.avatar = req.body.imgname;
       checkData.addUser(user).then(function(resp){
         console.log(resp);
         if(resp)
         res.send('already added');
         else{
+          var base64Data = decodeBase64Image(req.body.imgdata);
+          fs.closeSync(fs.openSync('./uploads/'+req.body.imgname, 'w'));
+          fs.writeFile('./uploads/'+req.body.imgname,base64Data.data, function(err) {
+            if(err) {
+              return console.log(err);
+            }
+            console.log("The file was saved!");
+          });
           roomsController(app);
           res.send('success');
         }
