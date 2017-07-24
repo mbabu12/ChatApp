bd85af7c4f88d1ed4777bf9a20b179a2684462e4
Maar Babu <Home@192.168.0.101>
2017-07-25 02:43:19 +0400

    creates awesome chat

--- assets/chat.js
+++ assets/chat.js
@@ -1,10 +1,80 @@
 var socket = io.connect('http://localhost:3000');
+var chatwith = '';
+var chatuser = '';
+
+$(document).ready(function(){
+  $('#enter').on('click', sendmsg);
+});
+
 socket.on('connect', function(){
   var userid = document.getElementById("jsfile").getAttribute("userid");
-  socket.emit('authentication', {id: userid });
+  var userName = document.getElementById("jsfile").getAttribute("userName");
+  socket.emit('authentication', {id: userid, name: userName });
   socket.on('authenticated', function() {
     // use the socket as usual
+    socket.on('getUsers', function(data){
+      //console.log(data);
+      var active = $('#active-users');
+      active.html('');
+      for (var i = 0; i < data.length; i++){
+        active.append('<hr>');
+        active.append('<li id = '+data[i].id +' onclick = startchat(this.id)><p>'+data[i].name+'</p></li>');
+      }
+      active.append('<hr>');
+    });
+    socket.emit('new connection','sdfsdf');
+
+    socket.on('sendmsg', function(data){
+      console.log(data.usId);
+      console.log(chatwith);
+      if(data.usId === chatwith){
+
+        var textarea = $('#texts');
+        textarea.append('<p>'+chatuser+': '+data.msg+'</p>');
+        scrollbottom();
+      }
+      else{
+        var clicked = document.getElementById(data.usId);
+        clicked.style.backgroundColor = 'lightblue';
+      }
+    });
   });
+});
+
+function startchat(id){
+  var clicked = document.getElementById(id);
+  clicked.style.backgroundColor = 'white';
+  chatwith = id;
+  var name = $('#'+id+" p").text();
+  chatuser = name;
+  var text = $('#header');
+  text.html('');
+  text.append('<h2>Chatting with '+name+'</h2>');
+  var textarea = $('#texts');
+  textarea.html('');
+}
+
+function scrollbottom(){
+  var objDiv = document.getElementById("texts");
+  objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+function sendmsg(){
+  if(chatwith !== ''){
+    var msgarea = $('#msgarea');
+    var msg = msgarea.val();
+    $('#texts').append('<p id = me >Me: '+msg+'</p>');
+    scrollbottom();
+    msgarea.val('');
+    socket.emit("newmsg", {to: chatwith, message:msg});
+  }
+}
+
+document.getElementById("msgarea").addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    document.getElementById("enter").click();
+  }
 });
 
 
--- controllers/chatController.js
+++ controllers/chatController.js
@@ -14,7 +14,7 @@ module.exports = function(app, io){
           req.session.reset();
           res.redirect('/');
         } else {
-          res.render('chat', {userId:req.session.user._id});
+          res.render('chat', {userId:req.session.user._id, userName:user.userName});
         }
       });
     }else{
@@ -26,7 +26,7 @@ require('socketio-auth')(io, {
   authenticate: function (socket, data, callback) {
     //get credentials sent by the client
     var userId = data.id;
-    connections[userId] = socket;
+    connections[userId] = {sock:socket, userName: data.name};
     allsockets[socket.id] = userId;
     return callback(null, true);
   }
@@ -34,13 +34,35 @@ require('socketio-auth')(io, {
 
   io.on('connect', function(socket){
     console.log("made socket connection");
-    
+    socket.on('new connection', function(){
+      sendUser(io);
+    });
+
+    socket.on('newmsg', function(data){
+      userId = data.to;
+      sockId = connections[data.to].sock.id;
+      connections[data.to].sock.emit('sendmsg', {msg:data.message, usId: allsockets[socket.id]});
+    });
+
   //  console.log(req.session.user);
     socket.on('disconnect', function(){
       var usId = allsockets[socket.id];
       delete connections[usId];
       delete allsockets[socket.id];
-      console.log('disconnected'+usId+" "+socket.id);
+      console.log('disconnected '+usId+" "+socket.id);
+      sendUser(io);
     });
   });
+
+  function sendUser(io){
+    for(connects in connections){
+      var active = [];
+      for(connect in connections){
+        if(connect !== connects){
+          active.push({name:connections[connect].userName,id:connect});
+        }
+      }
+      connections[connects].sock.emit('getUsers', active);
+    }
+  }
 }
